09a0d3f0a6bb3dcf40972aa9c4ffc1d07a852265
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-05-22 14:40:26 -0700

    copied the rumbl Source Tree

---
+++ apps/rumbl/lib/rumbl/multimedia/video.ex
@@ -0,0 +1,49 @@
+defmodule Rumbl.Multimedia.Video do
+  use Ecto.Schema
+  import Ecto.Changeset
+  @primary_key {:id, Rumbl.Multimedia.Permalink, autogenerate: true}
+
+  schema "videos" do
+    field :description, :string
+    field :title, :string
+    field :url, :string
+    field :slug, :string
+
+    belongs_to :user, Rumbl.Accounts.User
+    belongs_to :category, Rumbl.Multimedia.Category
+
+    has_many :annotations, Rumbl.Multimedia.Annotation
+
+    timestamps()
+  end
+
+  @spec changeset(
+          {map, map}
+          | %{
+              :__struct__ => atom | %{:__changeset__ => map, optional(any) => any},
+              optional(atom) => any
+            },
+          :invalid | %{optional(:__struct__) => none, optional(atom | binary) => any}
+        ) :: Ecto.Changeset.t()
+  @doc false
+  def changeset(video, attrs) do
+    video
+    |> cast(attrs, [:url, :title, :description, :category_id])
+    |> validate_required([:url, :title, :description])
+    |> assoc_constraint(:category)
+    |> slugify_title()
+  end
+
+  defp slugify_title(changeset) do
+    case fetch_change(changeset, :title) do
+      {:ok, new_title} -> put_change(changeset, :slug, slugify(new_title))
+      :error -> changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+end
