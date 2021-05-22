78b5ec61759b7333e6058fa5a171ae7ecb8f7d45
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-05-22 15:47:40 -0700

    copied the Web Source Files

--- apps/rumbl_web/lib/rumbl_web/router.ex
+++ apps/rumbl_web/lib/rumbl_web/router.ex
@@ -7,6 +7,7 @@ defmodule RumblWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug RumblWeb.Auth
   end
 
   pipeline :api do
@@ -17,6 +18,9 @@ defmodule RumblWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
+    get "/watch/:id", WatchController, :show
   end
 
   # Other scopes may use custom stacks.
@@ -38,5 +42,11 @@ defmodule RumblWeb.Router do
       pipe_through :browser
       live_dashboard "/dashboard", metrics: RumblWeb.Telemetry
     end
+  end
+
+  scope "/manage", RumblWeb do
+    pipe_through [:browser, :authenticate_user]
+
+    resources "/videos", VideoController
   end
 end
