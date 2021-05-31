5cbbab274c9860ac842430c892b608649b84d1a2
Cornelia Kelinske <corneliakelinske@gmail.com>
2021-05-31 14:16:29 -0700

    something not working with my infosys.compute

--- apps/info_sys/test/test_helper.exs
+++ apps/info_sys/test/test_helper.exs
@@ -1,2 +1,2 @@
-Code.require_file "../../info_sys/test/backends/http_client.exs", __DIR__
+Code.require_file "/home/connie/daeaev/rumbl_umbrella/apps/info_sys/test/backends/http_client.exs", __DIR__
 ExUnit.start()
